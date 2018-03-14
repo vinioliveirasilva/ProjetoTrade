639143fa94fb279e1725f075063d8ed006692adc
Vinicius Oliveira <vinicius.silva@intelitrader.com.br>
2018-03-14 14:12:14 -0300

    Adiciona botoes de navegação na tela
    Adiciona 4 botoes navegação na parte inferior da tela
    Cria classe que gera o grafico de pizza

--- ProjetoTrade/Activities/MainActivity.cs
+++ ProjetoTrade/Activities/MainActivity.cs
@@ -3,80 +3,43 @@ using Android.Widget;
 using Android.OS;
 using OxyPlot;
 using OxyPlot.Xamarin.Android;
-using OxyPlot.Series;
-using Android.Views;
+using System.Collections.Generic;
+using ProjetoTrade.Models;
+using ActionBar = Android.Support.V7.App.ActionBar;
 
 namespace ProjetoTrade
 {
     [Activity(Label = "ProjetoTrade", MainLauncher = true, Icon = "@drawable/Icon")]
     public class MainActivity : Activity
     {
-        private PlotView plotViewModel;
-        private LinearLayout mLLayoutModel;
-        public PlotModel MyModel { get; set; }
+        private PlotView plotView;
 
-        private int[] modelAllocValues = new int[] { 12, 5, 2, 40, 40, 1 };
-        private string[] modelAllocations = new string[] { "Slice1", "Slice2", "Slice3", "Slice4", "Slice5", "Slice6" };
-        string[] colors = new string[] { "#7DA137", "#6EA6F3", "#999999", "#3B8DA5", "#F0BA22", "#EC8542" };
-        int total = 0;
+        List<GraphModel> instrument;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-            plotViewModel = FindViewById<PlotView>(Resource.Id.plotViewModel);
-            mLLayoutModel = FindViewById<LinearLayout>(Resource.Id.linearLayoutModel);
 
-            //Model Allocation Pie char
-            var plotModel2 = new PlotModel();
-            var pieSeries2 = new PieSeries();
-            pieSeries2.InsideLabelPosition = 0.0;
-            pieSeries2.InsideLabelFormat = null;
+            //ActionBar actionBar = getSupport
 
-            for (int i = 0; i < modelAllocations.Length && i < modelAllocValues.Length && i < colors.Length; i++)
-            {
+            plotView = FindViewById<PlotView>(Resource.Id.plotViewModel);
 
-                pieSeries2.Slices.Add(new PieSlice(modelAllocations[i], modelAllocValues[i]) { Fill = OxyColor.Parse(colors[i]) });
-                pieSeries2.OutsideLabelFormat = null;
+            instrument = new List<GraphModel>();
+            instrument.Add(new GraphModel(40, "PETR4", "#7DA137"));
+            instrument.Add(new GraphModel(40, "PETR3", "#6EA6F3"));
+            instrument.Add(new GraphModel(40, "VALE5", "#999999"));
+            instrument.Add(new GraphModel(40, "BBAS3", "#3B8DA5"));
+            instrument.Add(new GraphModel(40, "VALE3", "#F0BA22"));
 
-                double mValue = modelAllocValues[i];
-                double percentValue = (mValue / total) * 100;
-                string percent = percentValue.ToString("#.##");
+            var graphPie = new GraphHelper(plotView, instrument, (s, e) => PieSeries_TouchStarted(s, e));
 
-                //Add horizontal layout for titles and colors of slices
-                LinearLayout hLayot = new LinearLayout(this);
-                hLayot.Orientation = Android.Widget.Orientation.Horizontal;
-                LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WrapContent, LinearLayout.LayoutParams.WrapContent);
-                hLayot.LayoutParameters = param;
+            plotView.Model = graphPie.getModel();
+        }
 
-                //Add views with colors
-                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(15, 15);
-
-                View mView = new View(this);
-                lp.TopMargin = 5;
-                mView.LayoutParameters = lp;
-                mView.SetBackgroundColor(Android.Graphics.Color.ParseColor(colors[i]));
-
-                //Add titles
-                TextView label = new TextView(this);
-                label.TextSize = 10;
-                label.SetTextColor(Android.Graphics.Color.Black);
-                label.Text = string.Join(" ", modelAllocations[i]);
-                param.LeftMargin = 8;
-                label.LayoutParameters = param;
-
-                hLayot.AddView(mView);
-                hLayot.AddView(label);
-                mLLayoutModel.AddView(hLayot);
-
-            }
-
-            plotModel2.Series.Add(pieSeries2);
-            MyModel = plotModel2;
-            plotViewModel.Model = MyModel;
-
+        private void PieSeries_TouchStarted(object sender, OxyTouchEventArgs e)
+        {
+            Toast.MakeText(ApplicationContext, "Clicked", ToastLength.Long).Show();
         }
     }
 }
---
+++ ProjetoTrade/Models/GraphHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using OxyPlot;
+using OxyPlot.Xamarin.Android;
+using OxyPlot.Series;
+
+namespace ProjetoTrade.Models
+{
+    class GraphHelper
+    {
+        List<GraphModel> _toPlot;
+        View _view;
+        PlotModel _plotModel;
+        PieSeries _pieSeries;
+
+        public GraphHelper(PlotView view, List<GraphModel> toPlot, EventHandler<OxyTouchEventArgs> onClick = null)
+        {
+            _view = view;
+            _toPlot = toPlot;
+            _plotModel = new PlotModel();
+            _pieSeries = new PieSeries();
+            
+            AddRange(ToSlice(_toPlot));
+
+            _plotModel.Series.Add(_pieSeries);
+            _pieSeries.TouchStarted += onClick;
+        }
+
+        void AddRange(List<PieSlice> list)
+        {
+            foreach (var toAdd in list)
+                _pieSeries.Slices.Add(toAdd);
+        }
+
+        public PlotModel getModel() => _plotModel;
+        void Add(PieSlice toAdd) => _pieSeries.Slices.Add(toAdd);
+        void Add(GraphModel toAdd) => _pieSeries.Slices.Add(ToSlice(toAdd));
+        private PieSlice ToSlice(GraphModel slice) => new PieSlice(slice.Name, slice.Value) { Fill = OxyColor.Parse(slice.Color) };
+
+        private List<PieSlice> ToSlice(List<GraphModel> slices)
+        {
+            var toReturn = new List<PieSlice>();
+            foreach (var slice in slices)
+                toReturn.Add(new PieSlice(slice.Name, slice.Value) { Fill = OxyColor.Parse(slice.Color) });
+            return toReturn;
+        }
+
+
+
+        // pieSeries.OutsideLabelFormat = null;
+
+        /*
+        //Add horizontal layout for titles and colors of slices
+
+        //=======================================================================
+        LinearLayout hLayot = new LinearLayout(this);
+        hLayot.Orientation = Orientation.Horizontal;
+        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WrapContent, LinearLayout.LayoutParams.WrapContent);
+        hLayot.LayoutParameters = param;
+
+        //Add views with colors
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(35, 35);
+
+        View mView = new View(this);
+        lp.TopMargin = 5;
+        mView.LayoutParameters = lp;
+        mView.SetBackgroundColor(Android.Graphics.Color.ParseColor(colors[i]));
+
+        //Add titles
+        TextView label = new TextView(this);
+        label.TextSize = 12;
+        label.SetTextColor(Android.Graphics.Color.Black);
+        label.Text = string.Join(" ", modelAllocations[i]);
+        param.LeftMargin = 15;
+        label.LayoutParameters = param;
+        //========================================================================
+
+        hLayot.AddView(mView); //Quadrado
+        hLayot.AddView(label); //Label
+        mLLayoutModel.AddView(hLayot); //adiciona Quadrado + Label
+        */
+    }
+
+    class GraphModel
+    {
+        public int Value { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+        public GraphModel(int value, string name, string color)
+        {
+            Value = value;
+            Name = name;
+            Color = color;
+        }
+    }
+}
